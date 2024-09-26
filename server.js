5184cd242d0080a7cebd4c187e4bc06136f7fdbe
PhamSang1210 <phamhongsang12x10@gmail.com>
2024-09-26 10:05:56 +0700

    port

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ cloudinary.v2.config({
 });
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server listening on port: ${process.env.PORT}`);
+    console.log(`Server listening on port: ${process.env.PORT || 5000}`);
 });
